8e7989be2fe96f2a2b2c085814b2ee5b75564acc
aitbali <aitbali@hotmail.com>
2022-11-12 18:31:51 +0100

    configure factories tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_12_165808) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_12_170754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
